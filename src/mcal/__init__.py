093ae6b783dc28e573b2da8896b3259790558c5c
subaru <subaru519kqka@gmail.com>
2026-02-25 23:08:40 +0900

    [update] add PySCF support for reorganization energy calculations and update version to 0.4.0

--- src/mcal/__init__.py
+++ src/mcal/__init__.py
@@ -1,1 +1,55 @@
+from .calculations.hopping_mobility_model import (
+    cal_pinv,
+    marcus_rate,
+    mobility_tensor,
+    diffusion_coefficient_tensor
+)
+from .calculations.rcal import Rcal
+from .utils.cif_reader import CifReader
+from .utils.gjf_maker import GjfMaker
+from .utils.gaus_log_reader import FileReader, check_normal_termination
+from .mcal import (
+    atom_weight,
+    cal_cen_of_weight,
+    cal_distance_between_cen_of_weight,
+    cal_min_distance,
+    cal_moment_of_inertia,
+)
 
+
+try:
+    from .calculations.rcal_pyscf import RcalPySCF
+    __all__ = [
+        'cal_pinv',
+        'marcus_rate',
+        'mobility_tensor',
+        'diffusion_coefficient_tensor',
+        'Rcal',
+        'RcalPySCF',
+        'CifReader',
+        'GjfMaker',
+        'FileReader',
+        'check_normal_termination',
+        'atom_weight',
+        'cal_cen_of_weight',
+        'cal_distance_between_cen_of_weight',
+        'cal_min_distance',
+        'cal_moment_of_inertia',
+    ]
+except ImportError:
+    __all__ = [
+        'cal_pinv',
+        'marcus_rate',
+        'mobility_tensor',
+        'diffusion_coefficient_tensor',
+        'Rcal',
+        'CifReader',
+        'GjfMaker',
+        'FileReader',
+        'check_normal_termination',
+        'atom_weight',
+        'cal_cen_of_weight',
+        'cal_distance_between_cen_of_weight',
+        'cal_min_distance',
+        'cal_moment_of_inertia',
+    ]
